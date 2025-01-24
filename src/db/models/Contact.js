2f9005854cb64ed6d490c24b2679e33b5902c429
Kateryna <katerynawalls@proton.me>
2025-01-24 17:11:01 -0600

    formate

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,38 +1,39 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
+const contactSchema = new Schema(
+  {
     name: {
-        type: String, 
-        required: true,
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
     },
 
-phoneNumber: {
-type: String, 
-required: true,
-},
+    email: {
+      type: String,
+      required: true,
+    },
 
-email: {
-    type: String,
-    required: true,
-},
+    isFavourite: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
 
-isFavourite: {
-    type: Boolean, 
-    default: false,
-    required: true,
-},
-
-contactType: {
-    type: String, 
-    enum: ["work", "home", "personal"], 
-    required: true,
-    default: "personal",
-},
-},
-{
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
     timestamps: true,
-});
+  },
+);
 
-const ContactCollection = model("contact", contactSchema);
+const ContactCollection = model('contact', contactSchema);
 
 export default ContactCollection;
