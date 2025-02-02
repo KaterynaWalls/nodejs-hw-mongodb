f99370d79ca4391e4cde6ea4270093c53d8af9b1
Kateryna <katerynawalls@proton.me>
2025-02-02 00:10:52 -0600

    update file

--- src/server.js
+++ src/server.js
@@ -8,13 +8,13 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
+  app.use(logger);
   app.use(cors());
   app.use(express.json());
 
   app.use('/contacts', contactsRouter);
-  app.use(errorHandler);
   app.use(notFoundHandler);
-  app.use(logger);
+  app.use(errorHandler);
 
   const port = Number(getEnvVar('PORT', 3000));
   app.listen(port, () => console.log(`Server running on ${port} port`));
